9fd2af350937907f609ebf08be2514a9af67e412
Florian Gattung <florian@shellfire.de>
2026-03-22 08:46:25 +0100

    test: Disable rich formatting in tests to fix CI errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
 import os
 
+# Used to prevent rich from wrapping output
 os.environ["TERMINAL_WIDTH"] = "10000"
+
+# Used to prevent rich from forcing color output in CI (like GITHUB_ACTIONS)
+os.environ["NO_COLOR"] = "1"
+os.environ["TERM"] = "dumb"
+os.environ.pop("GITHUB_ACTIONS", None)
+os.environ.pop("FORCE_COLOR", None)
